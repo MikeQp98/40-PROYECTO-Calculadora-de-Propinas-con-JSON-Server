7d7af93882729ba6c1f96f5169ce1993528e352b
Michael Peters <michaelrespond99@gmail.com>
2022-03-19 07:59:17 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,39 @@
+let cliente = {
+    mesa: '',
+    hora:'',
+    pedido: []
+};
+
+const btnGuardarCliente = document.querySelector('#guardar-cliente');
+btnGuardarCliente.addEventListener('click', guardarCliente);
+
+function guardarCliente() {
+    const mesa = document.querySelector('#mesa').value
+    const hora = document.querySelector('#hora').value
+    const camposVacios = [mesa, hora].some(campo => campo === '');
+
+    if(camposVacios) {
+        
+        const existeAlerta = document.querySelector('.invalid-feedback');
+        
+        if(!existeAlerta) {
+            const alerta = document.createElement('DIV');
+            alerta.classList.add('invalid-feedback', 'd-block', 'text-center');
+            alerta.textContent = 'Todos los campos son obligatorios';
+            document.querySelector('.modal-body form').appendChild(alerta);
+      
+            setTimeout(() => {
+                alerta.remove();
+            }, 3000);
+            
+        }
+        return;
+    }
+
+    cliente = {...cliente, mesa, hora}
+
+    const modalFormulario = document.querySelector('#formulario')
+    const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
+    modalBootstrap.hide();
+
+}
