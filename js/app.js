ef4c90a594c1251c68239f5d82d39a853fb02d94
Michael Peters <michaelrespond99@gmail.com>
2022-03-19 08:46:42 -0400

    Avance 2

--- js/app.js
+++ js/app.js
@@ -4,6 +4,13 @@ let cliente = {
     pedido: []
 };
 
+
+const categorias = {
+    1 : 'Comida', 
+    2 : 'Bebidas',
+    3 : 'Postres'
+}
+
 const btnGuardarCliente = document.querySelector('#guardar-cliente');
 btnGuardarCliente.addEventListener('click', guardarCliente);
 
@@ -39,11 +46,55 @@ function guardarCliente() {
     const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
     modalBootstrap.hide();
 
+    //Mostrar los productos
     mostrarSecciones();
+
+    //Obtener los datos de la api
+    obtenerPlatillos();
 
 }
 
 function mostrarSecciones(){
     const seccionesOcultas = document.querySelectorAll('.d-none');
     seccionesOcultas.forEach(seccion => seccion.classList.remove('d-none'));
+}
+
+function obtenerPlatillos() {
+
+    const url = ' http://localhost:4000/platillos'
+
+    fetch(url) 
+    .then(respuesta => respuesta.json())
+     .then(resultado => mostrarPlatillos(resultado))
+       .catch(error => console.log(error))
+    
+}
+
+function mostrarPlatillos(platillos) {
+
+    const contenido = document.querySelector('#platillos .contenido');
+
+    platillos.forEach( platillo => {
+
+        const row = document.createElement('DIV');
+        row.classList.add('row', 'py-3', 'border-top');
+
+        const nombre = document.createElement('DIV')
+        nombre.classList.add('col-md-4');
+        nombre.textContent = platillo.nombre;
+
+        const precio = document.createElement('DIV')
+        precio.classList.add('col-md-3','fw-bold');
+        precio.textContent = `$ ${platillo.precio}`
+
+        const categoria = document.createElement('DIV');
+        categoria.classList.add('col-md-3');
+        categoria.textContent = categorias[platillo.categoria];
+
+        row.appendChild(nombre);
+        row.appendChild(categoria);
+        row.appendChild(precio);
+        contenido.appendChild(row);
+    })
+
 }
